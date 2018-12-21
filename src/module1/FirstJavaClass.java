63d87142b1ad378fee26d90e20e447736133f316
varsha2019b <varsha2019b@gmail.com>
2018-12-21 00:07:43 -0500

    First Selenium session

--- src/module1/FirstJavaClass.java
+++ src/module1/FirstJavaClass.java
@@ -4,7 +4,7 @@ public class FirstJavaClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Wlecome to Selenium Session");
+		System.out.println("Welcome to Selenium Session");
 	}
 
 }
