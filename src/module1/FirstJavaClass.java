0f63961c9b82049a0550d547e8271f0179823a03
dinesh <dinesh2016b@gmail.com>
2018-12-20 23:26:14 -0500

    First Selenium session

---
+++ src/module1/FirstJavaClass.java
@@ -0,0 +1,10 @@
+package module1;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Wlecome to Selenium Session");
+	}
+
+}
